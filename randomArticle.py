6bfcdb6b6341357654e1be63676fb8b11bd37919
cifinn <ciaranfinn07@aol.com>
2014-11-22 14:20:25 +0000

    Python Flask App
    Uses the SpunOut APIs to get articles and then it returns a random
    article via JSON

---
+++ randomArticle.py
@@ -0,0 +1,43 @@
+import json
+import urllib2
+import random
+from flask import Flask
+from bs4 import BeautifulSoup
+
+
+def randomizeArt():
+	categories = []
+	articles = []
+	catURL = urllib2.urlopen('http://api.spunout.ie/v1/categories')
+	response = json.load(catURL)
+
+	ourResult = response['categories']
+	for rs in ourResult:
+		categories.append(rs)
+     
+	randomCat = random.choice(categories)
+
+	articleURL = urllib2.urlopen('http://api.spunout.ie/v1/search/by_category/'+randomCat)
+	spuntOutArticles = json.load(articleURL)
+
+	articleOutput = spuntOutArticles['services']
+	for  article in articleOutput:
+		articleObject = {'title': article['title'],'info': article['info'],'page': article['page']}
+
+		articles.append(articleObject)
+
+
+	return json.dumps(random.choice(articles), sort_keys=True, indent=4,
+                          separators=(',', ': '))
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def jsonObjects():
+    return randomizeArt()
+
+if __name__ == '__main__':
+    app.run()
